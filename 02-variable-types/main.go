0743e2146bf7c8f6be0b75d1501c5bfc66c89a84
Bello Olamilekan <belloolamilekan661@gmail.com>
2026-09-17 10:13:22 +0100

    Add variables, types, zero values, and const lesson

---
+++ 02-variable-types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const pi = 3.14159
+
+func main() {
+	name := "Gopher"
+	age := 5
+
+	fmt.Println(name, age)
+
+	var score int
+	var label string
+	var active bool
+
+	fmt.Println(score, label, active)
+
+}
